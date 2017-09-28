f81fedcdb600565a81b55bc75ede9949187de231
Valentin Le Blan <valentinleblan@gmail.com>
2017-09-28 17:51:48 +0200

    Exo 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'questions_controller/answer'
+  # get 'questions_controller/ask'
+  get 'answer', to: 'questions_controller#answer'
+  get 'ask', to: 'questions_controller#ask'
+  get 'answer/:query', to: 'questions_controller#answser'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
